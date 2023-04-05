487c1332a21063911254935b0ca7f81592ec0c2d
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-04-05 13:27:22 -0400

    qediffdrive is not done!

--- src/main/java/frc/qefrc/qelib/drive/QEDiffDrive.java
+++ src/main/java/frc/qefrc/qelib/drive/QEDiffDrive.java
@@ -1,7 +1,10 @@
+/* Copyright (c) 2023 Urban Inspire Corp 501(c)3 d.b.a. Questionable Engineering. All rights reserved. */
+/* This work is licensed under the terms of the MPL 2.0 license */
+/* found in the root directory of this project. */
 package frc.qefrc.qelib.drive;
 
 /**
- * QE DiffDrive Class
+ * QE DiffDrive Class NOT DONE DO NOT USE <br>
+ * </br>
  */
-public class QEDiffDrive {
-}
+public class QEDiffDrive {}
