929e0a0517c2645dfb763820a6474ea01defe51a
henrikvtcodes <commits@henrikvt.com>
2024-01-31 16:31:50 -0500

    clone vendordep template project, delete all native code, among other things

---
+++ src/main/java/com/orangeunilabs/roboled/RoboLED.java
@@ -0,0 +1,10 @@
+package com.orangeunilabs.roboled;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+
+/** Hi */
+public class RoboLED extends AddressableLED {
+    public RoboLED(int port) {
+        super(port);
+    }
+}
