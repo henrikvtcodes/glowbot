911197e2c4aa7d4a338ae95d4fc28b16ce8ed90a
henrikvtcodes <commits@henrikvt.com>
2024-02-26 14:47:47 -0500

    Implement the pattern interface and the section thing

--- src/main/java/com/orangeunilabs/glowbot/pattern/LEDPattern.java
+++ src/main/java/com/orangeunilabs/glowbot/pattern/LEDPattern.java
@@ -1,4 +1,10 @@
 package com.orangeunilabs.glowbot.pattern;
 
-public class LEDPattern {
+import com.orangeunilabs.glowbot.GlowbotControllable;
+
+/**
+ * All patterns to be used with {}
+ */
+public interface LEDPattern {
+    void runPattern(GlowbotControllable section, boolean isFirstRun);
 }
